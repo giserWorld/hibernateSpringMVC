66cce2773ae9cda30b1602e0460c57da7de71e47
wxt <602574436@qq.com>
2020-04-09 00:32:33 +0800

    测试hql语句

--- src/hibernate/com/test/test/Test_db_hql.java
+++ src/hibernate/com/test/test/Test_db_hql.java
@@ -3,18 +3,20 @@ package hibernate.com.test.test;
 import hibernate.com.test.dataSouce.DataConnecton;
 import hibernate.com.test.entity.User;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.junit.Test;
 
-//测试hibernate 
+/***********************hql查询***********************
+ *1. 
+ */
 public class Test_db_hql {
 
 	@Test
-	//测试select语句-hql
+	//hql-查询语句
 	public void testSelect(){
 		Session session = null;
 		try {
@@ -27,6 +29,27 @@ public class Test_db_hql {
 			//获取第一条数据
 			User user=(User)list.get(0);
 			System.out.println("名字:"+user.getName());
+		}
+		finally{
+			if(session!=null){
+				session.close();
+			}
+		}
+	}
+	
+	@Test
+	//hql-插入语句
+	public void hql_insertData(){
+		Session session = null;
+		try {
+			//获取session
+			session=DataConnecton.getSession();
+			Transaction tx=session.beginTransaction();
+			User newUser=new User();
+			newUser.setId(3);
+			newUser.setName("小李");
+			session.save(newUser);
+			tx.commit();//事务提交
 		}
 		finally{
 			if(session!=null){
